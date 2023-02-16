dc720071af12da1d48547bcbc8be443999311fa8
Marker22 <mrksbasha@gmail.com>
2023-02-16 15:17:56 -0800

    sizes of data

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("size of a char: %c byte(s)", sizeof(char));
-	printf("size of an int: %d byte(s)", sizeof(int));
-	printf("size of a long int: %ld byte(s)", sizeof(long int));
-	printf("size of a long long int: %lld byte(s)", sizeof(long long int));
-	printf("size of a float: %f byte(s)", sizeof(float));
+	printf("size of a char: %ld byte(s)", sizeof(char));
+	printf("size of an int: %ld byte(s)", sizeof(int));
+	printf("size of a long int: %lu byte(s)", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)", sizeof(long long int));
+	printf("size of a float: %lu byte(s)", sizeof(float));
 	return (0);
 }
