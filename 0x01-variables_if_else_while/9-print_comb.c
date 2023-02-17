e4cf8a1805731dec834e226945268a75af6c5afa
Marker22 <mrksbasha@gmail.com>
2023-02-17 09:23:53 -0800

    number separated by comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - a program main function is print number separated by comma
+ *
+ * Return: 0 when a program is successful launched
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
