53fd773606ae8267ba7218a4d8f4183be21b8b54
Marker22 <mrksbasha@gmail.com>
2023-02-21 12:45:00 -0800

    fibonacci 104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,39 +10,24 @@
 
 int main(void)
 {
-	int i;
-	unsigned long int a = 0, b = 1, sum;
-	unsigned long int j, k, l, m;
-	unsigned long int first, second;
+        unsigned long int t0 = 0, t1 = 1, s;
 
-	for (i = 0; i < 92; i++)
-	{
-		sum = a + b;
-		printf("%lu, ", sum);
-		a = b;
-		b = sum;
-	}
-	j = a / 1000000000;
-	l = b / 1000000000;
-	k = a % 1000000000;
-	m = b % 1000000000;
-	for (i = 93; i < 98; i++)
-	{
-		first = j + l;
-		second = k + m;
-		if (k + m > 999999999)
-		{
-			first += 1;
-			second %= 1000000000;
-		}
-		printf("%lu%lu", first, second);
-		if (i != 98)
-		printf(",");
-		j = l;
-		k = m;
-		l = first;
-		m = second;
-	}
-	printf("\n");
-	return (0);
+        int i;
+
+        for (i = 1; i<= 98; i++)
+        {
+                s = t0 + t1;
+
+                if (i != 98)
+                {
+                        printf("%lu, ", s);
+                }
+                else
+                {
+                        printf("%lu\n", s);
+                }
+                t0 = t1;
+                t1 = s;
+        }
+        return (0);
 }
